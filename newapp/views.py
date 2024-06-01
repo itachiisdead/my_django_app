53662a6065896addc29d1211e90750406b831311
hebx <hebamaher0101@gmail.com>
2024-06-01 16:07:14 +0200

    new page

--- newapp/views.py
+++ newapp/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    context={
+        'name': 'heba',
+        'age': 20,
+    }
+    return render(request, 'index.html', context)
+
+def about(request):
+    text= request.GET['texts']
+    num_of_words=len(text.split())
+    return render(request, 'about.html', {'num_of_words': num_of_words})
